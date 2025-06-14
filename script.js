951c3e3e69a4003ecffac05511ff2d15fa360c46
ArnavChandra2307 <arnavchandra360@gmail.com>
2025-06-14 11:32:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ const books = [
     cover: "covers/silence.jpg",
     page: "books/silence.html"
   },
+  {
+    title: "Silent Script: Magical Words OF Transformation",
+    description:"My name is Aradhya Gupta, and I have always believed in the quiet magic that words carry. They don’t just speak — they touch, they heal, and they transform. In my own journey through life’s ups and downs, I’ve often turned to words to find clarity, strength, and light. Over the past months, I’ve gathered fragments of thoughts — reflections scribbled in notebooks, moments whispered to the wind, and emotions that echoed in silence. These are not just quotes; they are pieces of my soul, born from lived experiences, deep observations, and a desire to understand the world and myself better. This book is a collection of those moments. Each quote holds within it a lesson I’ve learned, a feeling I’ve embraced, or a truth I’ve uncovered. It’s raw. It’s honest. And most importantly, it’s real. If even one line here resonates with your heart or lights a small fire within you, then this journey has been worth it.",
+    cover: "covers/silentscript.png",
+    page:"books/silentscript.html",
+  }
   // Add more book objects here
 ];
 
